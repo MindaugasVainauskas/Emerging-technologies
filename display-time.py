a9122b2397183a0bce6f4f7287f629f5eafa813f
M.Vainauskas <mindelis1@gmail.com>
2017-09-21 21:56:50 +0100

    Added date time display file

---
+++ display-time.py
@@ -0,0 +1,12 @@
+# 21/09/2017
+# Python problem sheet
+# Problem 2 - Current time display
+
+#import date/time module
+import datetime
+
+#Set time variable to current date and time
+time = datetime.datetime.now()
+
+#print out the date/time
+print(time)
