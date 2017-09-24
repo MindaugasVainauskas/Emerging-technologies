cce4bf6b00608fa1be7a0ec5bdd5903e82d37fc1
M.Vainauskas <mindelis1@gmail.com>
2017-09-24 20:16:36 +0100

    Guessing game now records user guesses and displays them after game is finished. Still need to set it to only register same value once

--- guessing-game-problem.py
+++ guessing-game-problem.py
@@ -3,26 +3,36 @@
 # Guessing game
 
 import random
+import array
 
 #Variables declaration
 guessCorrect = 0
 
 #Get maximum random number available for player to guess
-maxRandom = int(input("Greetings! Please select maximum number to guess from: "))
+maxRandom = int(input("Greetings! Please select maximum positive number to guess from: "))
 
 print("Thank you. This number will be the maximum possible guess.")
 
 #Create random number
 randomNumber = random.randint(0, maxRandom)
 
+#create a list to hold guesses
+guessList = []
+
 while(guessCorrect == 0):
 	guess = int(input("Enter your guess: "))
 	
 	if(guess > randomNumber):
 		print("Your guess is larger than the random number!")
+		guessList.append(guess)
 	elif(guess < randomNumber):
 		print("Your guess is smaller than the random number!")
+		guessList.append(guess)
 	elif(guess == randomNumber):
 		print("Congratulations! Your guessed correctly!")
+		guessList.append(guess)
 		guessCorrect = 1
 
+#Print out the guesses from list
+print("Your guesses were:")
+print(guessList)
