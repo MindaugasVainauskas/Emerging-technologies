81c32cac3ad92844c171768e8efa0ebcca970bfa
M.Vainauskas <mindelis1@gmail.com>
2017-09-27 15:46:42 +0100

    Added string checking for palindrome. Still need to add comments and reference

--- test-if-palindrome.py
+++ test-if-palindrome.py
@@ -1,7 +1,21 @@
 # 27/09/2017
 #Python problem sheet. Problem 7
 # Test if String is a palindrome
+
+#define the palindrome test
+def isPalindrome(test):
+	test = test.lower()  #Put all letters into lowercase
+	
+	second = reversed(test)
+	
+	if(list(test) == list(second)):
+		print("String is Palindrome")
+	else:
+		print("String is not a Palindrome!")
+	
 print("Enter your string:")
 checkString = input()
 
-print(checkString)
+#Run a check if string is Palindrome
+isPalindrome(checkString)
+
