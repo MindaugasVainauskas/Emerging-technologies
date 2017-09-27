7ac4f60b96643a104dc96eb904301dfa56ec3b3e
M.Vainauskas <mindelis1@gmail.com>
2017-09-27 15:49:37 +0100

    Added comments and reference URL to site with palindrome solution

--- test-if-palindrome.py
+++ test-if-palindrome.py
@@ -6,13 +6,17 @@
 def isPalindrome(test):
 	test = test.lower()  #Put all letters into lowercase
 	
-	second = reversed(test)
+#Following is adapted from:
+# https://www.programiz.com/python-programming/examples/palindrome	
+	
+	second = reversed(test) #Temporary string is a reversed version of test string
 	
 	if(list(test) == list(second)):
 		print("String is Palindrome")
 	else:
 		print("String is not a Palindrome!")
 	
+#Get user to enter the string
 print("Enter your string:")
 checkString = input()
 
