70eb7a805e608da35febc6501fee88af57842ebe
M.Vainauskas <mindelis1@gmail.com>
2017-09-27 14:21:07 +0100

    Problem 6 is now finished and comments added

--- largest-smallest-problem.py
+++ largest-smallest-problem.py
@@ -1,3 +1,33 @@
 # 25/09/2017
 #Python problem sheet. Problem 6
-# Largest and smallest in the list
+# Largest and smallest in the list. I am assuming integer numbers since no specific data type was requested
+
+#declare sentinel value
+continueLoop = 1
+
+#declare list
+numList = []
+
+print("Welcome! This small program will find min and max number in your list")
+
+#run the program until sentinel value is entered
+while(continueLoop == 1):
+	num = int(input("Enter number to add to the list(-111 to exit): "))
+	
+	if(num == -111):
+		continueLoop = 0  #Sentinel value entered breaks the loop
+	else:
+		numList.append(num)  #No sentinel value means number is added to the list
+	
+	
+# Following are built in methods to find smallest and largest elements in list
+max = max(numList)
+min = min(numList)
+
+#print entire list
+print("Your entered number list is:")
+print(numList)
+
+#Print largest and smallest values
+print("Largest number in the list is: %d" % max)
+print("Smallest number in the list is: %d" % min)
