7fbecfd1910d50e9a39d2e10a18743ae6f54668a
M.Vainauskas <mindelis1@gmail.com>
2017-09-27 20:38:08 +0100

    Added comments for the file

--- reverse-string.py
+++ reverse-string.py
@@ -6,10 +6,9 @@
 print("Enter your string:")
 userString = input()
 
-#Reprint the user entered string
-print(userString)
-
 #Reverse the string
-reverseString = userString[::-1]
+reverseString = userString[::-1]  #Using extended slice syntax for a quick way of reversing the string
 
+#Print out reversed string
+print("\nYour string reversed is:")
 print(reverseString)
