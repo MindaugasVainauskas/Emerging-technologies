cf2ba0357f9ed0ffe221fa4b99d919a290c99234
M.Vainauskas <mindelis1@gmail.com>
2017-09-27 20:33:56 +0100

    Added problem 10 file. Added string input same as in problem 8. Using different way of reverse string as reversed() prints out reversed object instead of string itself

---
+++ reverse-string.py
@@ -0,0 +1,15 @@
+# 27/09/2017
+#Python problem sheet. Problem 10
+# Reverse a string and print it out
+
+#Get user to enter the string
+print("Enter your string:")
+userString = input()
+
+#Reprint the user entered string
+print(userString)
+
+#Reverse the string
+reverseString = userString[::-1]
+
+print(reverseString)
