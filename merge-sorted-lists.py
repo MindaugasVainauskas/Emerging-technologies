01e8eb9b6bd81973f105715a9f9a0ad82da69407
M.Vainauskas <mindelis1@gmail.com>
2017-09-27 14:47:41 +0100

    Added List population addition and sorting methods. All of functions used are built in which simplifies the process greatly.

--- merge-sorted-lists.py
+++ merge-sorted-lists.py
@@ -10,6 +10,7 @@ mergedList = []
 #Declare sentinel values
 sentinel = 1
 
+print("Populate list 1")
 #Populate list one
 while(sentinel == 1):
 	num1 = int(input("Enter number into list 1(-111 to exit): "))
@@ -19,6 +20,7 @@ while(sentinel == 1):
 	else:	
 		listOne.append(num1)
 		
+print("Populate list 2")
 #Populate list one
 while(sentinel == 0):
 	num2 = int(input("Enter number into list 2(-111 to exit): "))
@@ -27,9 +29,20 @@ while(sentinel == 0):
 		sentinel = 1
 	else:	
 		listTwo.append(num2)
-		
+
+#Sort the lists		
+listOne.sort()
+listTwo.sort()
+print("Lists sorted")
 #Print out the lists
 print("List 1:")
 print(listOne)
 print("List 2:")
 print(listTwo)
+
+#merge the lists to create new list
+mergedList = listOne + listTwo
+#sort this new list
+mergedList.sort()
+print("Sorted merged list")
+print(mergedList)
